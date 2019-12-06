c744363f0ae069a5c140e92ff9d2ea373ffef122
HasGand <1769811879@qq.com>
2019-12-06 13:20:32 +0800

    update

--- scripts_bin/run_transiesta-0.sh
+++ scripts_bin/run_transiesta-0.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #依次输入 计算核数 结构名称 各电压值
 path="/home/node/software/mpich/mpich/bin"
-echo -e "\n----------------------------------------------------------------------------------------------"
-echo "Welcome to use $0."
-echo -e "You have submitted $((($#-2))) tasks!\nThe parameters you enter are ( $* )."
-echo -e "The task number for this script is $$.\nYou can kill this IV-work.sh through the task number!."
-echo -e "----------------------------------------------------------------------------------------------\n\n"
+echo    ""
+echo    "|----------------------------------------------------------------------------------------------|"
+echo    "| 					Welcome to use $0.                                      |"
+echo -e "|	   You have submitted $((($#-2))) tasks!\nThe parameters you enter are ( $* ).          |"
+echo -e "|The task number for this script is $$.\nYou can kill this IV-work.sh through the task number!.|"
+echo -e "|----------------------------------------------------------------------------------------------|\n\n"
 sed -i "1,\$s/##/$2/g" doc/scat.fdf doc/tbtrans.fdf
 cd left
 $path/mpirun -np $1 transiesta < left.fdf > left.out
