2eebee633ab43bf20f3bb7278efaacd1779a7c91
HasGand <1769811879@qq.com>
2019-12-27 11:16:23 +0800

    update

--- scripts_bin/run_load.sh
+++ scripts_bin/run_load.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/sh
 echo ""
 echo -e "|-----------------------------------------------------------------------------------------|"
-echo -e "|		(*^_^*)The task number for this script is $$.				   |"
-echo -e "|		You can kill the $0 through the task number!.(￣y▽,￣)╭ 		   |"
+echo -e "|              (*^_^*)The task number for this script is $$.                               |"
+echo -e "|              You can kill the $0 through the task number!.(￣y▽,￣)╭             |"
 echo -e "|-----------------------------------------------------------------------------------------|"
 echo -e "\n"
 
@@ -10,12 +10,12 @@ commod=$*
 load=0.8
 while echo -e "need load average < $load ;当前uptime：$(uptime | awk '{print }')"
 do
-	if [[ `echo "$(uptime | awk '{printf("%f"), $9}') < $load" |bc` -eq 1 ]]
-	then
-		echo -e "\n\n\nNow, load average < $load\n\n\n"
-		echo -e `$commod >> commod.out`
-		break
-	fi
-	sleep 10
-	echo -e "after 10 sec..."
+        if [[ `echo "$(uptime |awk '{printf("%f",$(NF-1))}') < $load" |bc` -eq 1 ]]
+        then
+                echo -e "\n\n\nNow, load average < $load\n\n\n"
+                echo -e `$commod >> commod.out`
+                break
+        fi
+        sleep 10
+        echo -e "after 10 sec... load=$(uptime |awk '{printf("%f",$(NF-1))}')"
 done
