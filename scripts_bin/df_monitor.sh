15cdcac397154dfcb2f085d3099f8c5688b446b4
HasGand <1769811879@qq.com>
2020-01-04 17:11:15 +0800

    update

--- scripts_bin/df_monitor.sh
+++ scripts_bin/df_monitor.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/sh
-num=85
+num=1
 while true
 do
-	if [[ `echo "$(df -h |grep /home |awk '{printf("%d", $(NF-1))}') >= $num" |bc` -eq 1  ]]
+	if [[ `echo "$(df -h |grep /dev |awk '{printf("%d", $(NF-1))}') >= $num" |bc` -eq 1  ]]
 	then
-		echo -e "/home的硬盘使用率已达$num,请留意！" >> ~/attention_from_home
+		echo -e "/home的硬盘使用率已达${num}%,请留意！" >> ~/attention_from_home
 		break
 	fi
 	sleep 43200
